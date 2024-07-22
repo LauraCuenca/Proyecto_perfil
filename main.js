58a36298e22ba75ba55dc8b7e6a454b1a19b4c14
Cuenca Laura Antonina <laura.cuenca1@gmail.com>
2024-07-22 20:31:43 -0300

    agrego views

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+  fetch('components/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('navbar-container').innerHTML = data;
+    })
+    .catch(error => console.error('Error al cargar la navbar:', error));
+});
+
+
 let btnVerMasMelanie = document.getElementById("btnVerMasMelanie");  
 const containerMelanie = document.querySelector(".verMasMelanie");
 let btnVerMasLaura = document.getElementById("btnVerMasLaura");  
@@ -6,80 +16,75 @@ const containerLaura = document.querySelector(".verMasLaura");
     if(btnVerMasMelanie){
         btnVerMasMelanie.addEventListener("click",() =>{
 
-            containerMelanie.innerHTML+=
-            `<div class="col">
-                <div class="card">
-                <div class="card-body">
-                    <p class="card-text ">" Trabajo como supervisora en el área de Ingeniería en una empresa."</p>
+            containerMelanie.innerHTML += `
+            <div class="col">
+              <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                  <p class="card-text">Trabajo como supervisora en el área de Ingeniería en una empresa.</p>
                 </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                  <p class="card-text">Estudios avanzados en Ingeniería Electrónica.</p>
                 </div>
-            </div>
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <p class="card-text" >"Estudios avanzados en Ingeniería Electrónica."</p>
               </div>
             </div>
-          </div>
-          <div class="col">
-              <div class="card">
-              <div class="card-body ">
-                <p class="card-text p-3">"28 años, viviendo en Buenos Aires, CABA."</p>
-                </div>  
+            <div class="col">
+              <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                  <p class="card-text">28 años, viviendo en Buenos Aires, CABA.</p>
+                </div>
               </div>
             </div>
-          </div> 
-          <div class="col">
-              <div class="card">
-              <div class="card-body ">
-                <p class="card-text p-3">"Estudiando programación web con el objetivo de poder insertarme en el mundo IT."</p>
-                </div>  
+            <div class="col">
+              <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                  <p class="card-text">Estudiando programación web con el objetivo de poder insertarme en el mundo IT.</p>
+                </div>
+              </div>
+            </div>`;
+          btnVerMasMelanie.classList.add('d-none');
+      });
+  }
+    if (btnVerMasLaura) {
+      btnVerMasLaura.addEventListener("click", () => {
+          containerLaura.innerHTML += `
+            <div class="col">
+              <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                  <p class="card-text">Trabajo en comercio, de atención al público.</p>
+                </div>
               </div>
             </div>
-          </div> 
-            `
-            btnVerMasMelanie.classList.add('d-none');
-        });
-    
-    }
-    if(btnVerMasLaura){
-        btnVerMasLaura.addEventListener("click",() =>{
+            <div class="col">
+              <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                  <p class="card-text">Estudio Licenciatura en Sistemas, en la UNLP.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                  <p class="card-text">Tengo 30 años, vivo en La Plata.</p>
+                </div>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100">
+                <div class="card-body d-flex flex-column">
+                  <p class="card-text">Realizo varios cursos de programación para insertarme en el mundo IT.</p>
+                </div>
+              </div>
+            </div>`;
+          btnVerMasLaura.classList.add('d-none');
+      });
+  }
 
-            containerLaura.innerHTML+=
-            `<div class="col">
-                <div class="card">
-                <div class="card-body">
-                    <p class="card-text ">" Lisicing elit. Assumenda aliquid maiores."</p>
-                </div>
-                </div>
-            </div>
-          <div class="col">
-            <div class="card">
-              <div class="card-body">
-                <p class="card-text" >"Incidunt rerum a eaque consectetur esse quas! "</p>
-              </div>
-            </div>
-          </div>
-          <div class="col">
-              <div class="card">
-              <div class="card-body ">
-                <p class="card-text p-3">"Assumenda illum ab,  quas! "</p>
-                </div>  
-              </div>
-            </div>
-          </div> 
-          <div class="col">
-              <div class="card">
-              <div class="card-body ">
-                <p class="card-text p-3">"aperiam ipsam incidunt rerum a eaque consectetur esse"</p>
-                </div>  
-              </div>
-            </div>
-          </div> 
-            `
-            btnVerMasLaura.classList.add('d-none');
-        });    
-    }
+  
+  
     
     
 
